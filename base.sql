c27dd39183f6c538985af674b9aaa163293110bb
Ekifu <114438578+Ekifu@users.noreply.github.com>
2023-03-23 12:10:48 +0100

    Add files via upload

--- base.sql
+++ base.sql
@@ -12,6 +12,13 @@ CREATE TABLE deliversman(
     email varchar(50),
     PRIMARY KEY(id)
     );
+CREATE TABLE admin(
+    id int AUTO_INCREMENT ,
+    login_ varchar(50),
+    password_ varchar(50),
+    email varchar(50),
+    PRIMARY KEY(id)
+    );
 CREATE TABLE orders (
   id INT AUTO_INCREMENT PRIMARY KEY,
   user_id INT NOT NULL,
@@ -23,7 +30,7 @@ CREATE TABLE orders (
   date_ DATETIME NOT NULL
 );
 
-INSERT INTO `admin`(`login_`, `password_`, `email`) VALUES ('pizzaman','51d173b557122f3f9ded11fd8cccd0002ef48682','capriciosa@pizza.com')
-INSERT INTO `deliversman`(`login_`, `password_`, `email`) VALUES ('mario','f62c3dd976af0259a548da0c32bc665c5495e8eb','mario@pizza.com')
+INSERT INTO `admin`(`login_`, `password_`, `email`) VALUES ('pizzaman','51d173b557122f3f9ded11fd8cccd0002ef48682','capriciosa@pizza.com');
+INSERT INTO `deliversman`(`login_`, `password_`, `email`) VALUES ('mario','f62c3dd976af0259a548da0c32bc665c5495e8eb','mario@pizza.com');
 -- password_admin="capriciosa"
 -- password_deliversman="fiat"
